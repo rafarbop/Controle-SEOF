9807a0482fa8181218dec6409438534522977272
Rafa Bruno <67751655+rafarbop@users.noreply.github.com>
2021-04-14 18:50:51 -0300

    Read from Firebase ok

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,52 @@
-
-
-function keyAPI() {
-    const userKey = prompt("Informe a chave de acesso(token)  a ser utilizado na API:")
+function keyApiGov() {
+    const userKey = prompt("Informe a chave de acesso(token) a ser utilizado na API:")
     return userKey
 }
 
-document.getElementById("buttonConsultarEmpenho").onclick = () => {
+// Configuração do Firebase Realtime Database
+const keyUserFirebase = prompt("Informe a apiKey para acesso ao Firebase:")
+const firebaseConfig = {
+    apiKey: keyUserFirebase,
+    authDomain: "controle-empenhos.firebaseapp.com",
+    databaseURL: "https://controle-empenhos-default-rtdb.firebaseio.com",
+    projectId: "controle-empenhos",
+    storageBucket: "controle-empenhos.appspot.com",
+    messagingSenderId: "539608286640",
+    appId: "1:539608286640:web:0beb4fe688b33409ecc275"
+}
+// Inicializando o Firebase
+firebase.initializeApp(firebaseConfig)
+// Get a reference to the database service
+var database = firebase.database();
 
-    var numeroEmpenho = document.getElementById("numberEmpenho").value
+var lerEmpenho = database.ref('empenhos/')
+lerEmpenho.on('value',(listaEmpenhos) => {
 
-    if (numeroEmpenho.length != 12){
-        return alert("O Empenho deve ser informado com o ano e numero: Ex.: 2021NE000001")
-        
-    }
+    listaEmpenhos.forEach((empenho) => {
 
-    var requisicao = new XMLHttpRequest();
+        const empenhoJson = empenho.val()
 
-    var urlBaseAPI = "http://api.portaldatransparencia.gov.br/api-de-dados/despesas/documentos/15895326405"
-
-    var urlEmpenho = urlBaseAPI+numeroEmpenho
-
-    var keyUserAPI = keyAPI()
-
-    requisicao.open("GET", urlEmpenho);
-    requisicao.setRequestHeader("chave-api-dados", keyUserAPI);
-    requisicao.responseType = 'text';
-    requisicao.send();
-    console.log("Enviada solicitação e esperando Respota!")
-
-    requisicao.onload = () => {
-        alert("Resposta Recebida")
-        var jsonResponse = JSON.parse(requisicao.response)
-        console.log(jsonResponse)
-        var dados = document.getElementById("div")
-        for (key in jsonResponse) {
+        const dados = document.getElementById("listaDeEmpenhos")
+        for (key in empenhoJson) {
             let item = document.createElement("div");
-            item.classList.add("columns","has-text-centered","has-background-success-dark","has-text-white")
+            item.classList.add("columns", "has-text-centered", "has-background-success-dark", "has-text-white")
 
             let itemKey = document.createElement("div");
-            itemKey.classList.add("column","is-one-quarter")
+            itemKey.classList.add("column", "is-one-quarter")
             itemKey.innerText = key;
             item.appendChild(itemKey)
 
             let itemInfo = document.createElement("div");
-            itemInfo.classList.add("column","has-text-centered")
-            itemInfo.innerText = jsonResponse[key]
+            itemInfo.classList.add("column", "has-text-centered")
+            itemInfo.innerText = empenhoJson[key]
             item.appendChild(itemInfo)
-
+            
             document.getElementById("container-empenhos").insertBefore(item, dados)
+            
 
         }
-        console.log("Resposta Recebida!")
-    }
-    
+    })
 
-}
+    
+})
+
