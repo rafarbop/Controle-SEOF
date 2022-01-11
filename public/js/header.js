129f53a73ef2c0f3f7603ffb6425d2ae34cdac1a
Rafael Bruno <rafarbop@gmail.com>
2022-01-11 20:06:48 -0300

    Update header.js

--- public/js/header.js
+++ public/js/header.js
@@ -6,7 +6,7 @@ document.body.onload = () => {
     <nav class="has-background-success-dark" role="navigation" aria-label="main navigation">
         <div class="is-flex is-justify-content-space-evenly">
             <a href="./lista-empenhos.html" class="button is-primary is-inverted is-outlined">Lista de Empenhos</a>
-            <a href="/" class="button is-primary is-inverted is-outlined">Consultar Novos empenhos</a>
+            <a href="./index.html" class="button is-primary is-inverted is-outlined">Consultar Novos empenhos</a>
         </div>
     </nav>`
 }
