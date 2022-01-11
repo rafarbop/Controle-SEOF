fbfea891184c4f57b861a8cb90d9542dd36bbb2e
Rafael Bruno <rafarbop@gmail.com>
2022-01-11 19:52:22 -0300

    UPDATE: links and pages paths

--- public/js/header.js
+++ public/js/header.js
@@ -5,8 +5,8 @@ document.body.onload = () => {
     <h1 class="title box is-marginless has-background-success-dark has-text-success-light has-text-centered">Controle de Empenhos</h1>
     <nav class="has-background-success-dark" role="navigation" aria-label="main navigation">
         <div class="is-flex is-justify-content-space-evenly">
-            <a href="/" class="button is-primary is-inverted is-outlined">Lista de Empenhos</a>
-            <a href="/api-to-database.html" class="button is-primary is-inverted is-outlined">Consultar Novos empenhos</a>
+            <a href="./lista-empenhos.html" class="button is-primary is-inverted is-outlined">Lista de Empenhos</a>
+            <a href="/" class="button is-primary is-inverted is-outlined">Consultar Novos empenhos</a>
         </div>
     </nav>`
 }
--- public/js/index.js
+++ public/js/index.js
@@ -1,92 +1,72 @@
-function keyApiGov() {
-    const userKey = prompt("Informe a chave de acesso(token) a ser utilizado na API:")
-    return userKey
-}
-
-// Configuração do Firebase Realtime Database
-const keyUserFirebase = prompt("Informe a apiKey para acesso ao Firebase:")
-const firebaseConfig = {
-    apiKey: keyUserFirebase,
-    authDomain: "controle-empenhos.firebaseapp.com",
-    databaseURL: "https://controle-empenhos-default-rtdb.firebaseio.com",
-    projectId: "controle-empenhos",
-    storageBucket: "controle-empenhos.appspot.com",
-    messagingSenderId: "539608286640",
-    appId: "1:539608286640:web:0beb4fe688b33409ecc275"
-}
-// Inicializando o Firebase
-firebase.initializeApp(firebaseConfig)
-// Get a reference to the database service
-var database = firebase.database();
-
-var lerEmpenho = database.ref('empenhos/')
-lerEmpenho.on('value',(listaEmpenhos) => {
-
-    listaEmpenhos.forEach((empenho) => {
-
-        const empenhoJson = empenho.val()
-        console.log(empenhoJson.data)
-
-        const dados = document.getElementById("listaDeEmpenhos")
-
-        let groupColumns = document.createElement("div")
-        groupColumns.classList.add("columns","is-fullwidth","is-vcentered","has-text-centered","has-background-success","has-text-dark")
-
-        //Criando as colunas de cada empenho
-        let colValor = document.createElement("div")
-        colValor.classList.add("column","is-1")
-        let colObs = document.createElement("div")
-        colObs.classList.add("column","is-4","has-text-justified")
-        let colNomeFav = document.createElement("div")
-        colNomeFav.classList.add("column")
-        let colCodigoFav = document.createElement("div")
-        colCodigoFav.classList.add("column")
-        let colProcesso = document.createElement("div")
-        colProcesso.classList.add("column")
-        let colNumeroEmpenho = document.createElement("div")
-        colNumeroEmpenho.classList.add("column")
-        let colData = document.createElement("div")
-        colData.classList.add("column","is-1")
 
 
-        colValor.innerText = empenhoJson.valor
-        colObs.innerText = empenhoJson.observacao
-        colNomeFav.innerText = empenhoJson.nomeFavorecido
-        colCodigoFav.innerText = empenhoJson.codigoFavorecido
-        colProcesso.innerText = empenhoJson.numeroProcesso
-        colNumeroEmpenho.innerText = empenhoJson.documentoResumido
-        colData.innerText = empenhoJson.data
+document.getElementById("buttonConsultarEmpenho").onclick = () => {
+
+    var numeroEmpenho = document.getElementById("numberEmpenho").value
+
+    var keyApiGov = document.getElementById("keyApiGov").value
+
+    if (numeroEmpenho.length != 12){
+        return alert("O Empenho deve ser informado com o ano e numero: Ex.: 2021NE000001")
         
-        groupColumns.appendChild(colData)
-        groupColumns.appendChild(colNumeroEmpenho)
-        groupColumns.appendChild(colProcesso)
-        groupColumns.appendChild(colCodigoFav)
-        groupColumns.appendChild(colNomeFav)
-        groupColumns.appendChild(colObs)
-        groupColumns.appendChild(colValor)
+    }
 
-        dados.appendChild(groupColumns)
+    var requisicao = new XMLHttpRequest();
 
-        // for (key in empenhoJson) {
-        //     let item = document.createElement("div");
-        //     item.classList.add("columns", "has-text-centered", "has-background-success-dark", "has-text-white")
+    var urlBaseAPI = "http://api.portaldatransparencia.gov.br/api-de-dados/despesas/documentos/15895326405"
 
-        //     let itemKey = document.createElement("div");
-        //     itemKey.classList.add("column", "is-one-quarter")
-        //     itemKey.innerText = key;
-        //     item.appendChild(itemKey)
+    var urlEmpenho = urlBaseAPI+numeroEmpenho
 
-        //     let itemInfo = document.createElement("div");
-        //     itemInfo.classList.add("column", "has-text-centered")
-        //     itemInfo.innerText = empenhoJson[key]
-        //     item.appendChild(itemInfo)
-            
-        //     document.getElementById("container-empenhos").insertBefore(item, dados)
-            
+    requisicao.open("GET", urlEmpenho);
+    requisicao.setRequestHeader("chave-api-dados", keyApiGov);
+    requisicao.responseType = 'text';
+    requisicao.send();
+    console.log("Enviada solicitação e esperando Respota!")
 
-        // }
-    })
+    var dados = document.getElementById("listaDeEmpenhos")
+    dados.innerHTML = '<button class="button is-success is-loading is-rounded">Loading</button>'
 
+    requisicao.onload = () => {
+        
+        dados.innerHTML = ''
+
+        var jsonResponse = JSON.parse(requisicao.response)
+        console.log(jsonResponse)
+
+        var empenhoJson = {}
+
+        empenhoJson.data = jsonResponse.data
+        empenhoJson.documentoResumido = jsonResponse.documentoResumido
+        empenhoJson.numeroProcesso = jsonResponse.numeroProcesso
+        empenhoJson.codigoFavorecido = jsonResponse.codigoFavorecido
+        empenhoJson.nomeFavorecido = jsonResponse.nomeFavorecido
+        empenhoJson.valor = jsonResponse.valor
+        empenhoJson.observacao = jsonResponse.observacao
+
+        const containerEmpenhos = document.getElementById("container-empenhos")
+        while (containerEmpenhos.firstChild){
+            containerEmpenhos.removeChild(containerEmpenhos.firstChild)
+        }
+
+        for (key in empenhoJson) {
+            let item = document.createElement("div");
+            item.classList.add("columns","has-text-centered","has-background-success-dark","has-text-white")
+
+            let itemKey = document.createElement("div");
+            itemKey.classList.add("column","is-one-quarter")
+            itemKey.innerText = key;
+            item.appendChild(itemKey)
+
+            let itemInfo = document.createElement("div");
+            itemInfo.classList.add("column","has-text-centered")
+            itemInfo.innerText = empenhoJson[key]
+            item.appendChild(itemInfo)
+
+            containerEmpenhos.appendChild(item)
+
+        }
+        console.log("Resposta Recebida!")
+    }
     
-})
 
+}
